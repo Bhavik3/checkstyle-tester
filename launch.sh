963759df74ba2ae1fb49f111b0a0ec8e81051db6
Daniil Yaroslavtsev <dyaroslavtsev@reveredata.com>
2014-12-01 00:24:56 +0400

    Switched tester to use checkstyle:checkstyle goal

--- launch.sh
+++ launch.sh
@@ -33,7 +33,7 @@ else
 fi
 
 echo "Running Checkstyle on $SOURCES_DIR ..."
-time mvn checkstyle:checkstyle-aggregate "$@" # > target/console_log.txt
+time mvn checkstyle:checkstyle "$@" # > target/console_log.txt
 echo "Running Checkstyle on $SOURCES_DIR - finished"
 
 echo "Testing Checkstyle finished"
